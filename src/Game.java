61b674bc9023dfe4bab59960a3e8c692d2e9b642
Tyler <tylerrimkunas@gmail.com>
2024-04-25 17:39:23 -0400

    Implemented processTurn() and actionCard methods

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,3 @@
-import java.util.ArrayList;
-
 public class Game {
     private final Player[] players;
     private Deck deck;
@@ -44,5 +42,37 @@ public class Game {
         s.append("Last Card: ").append(deck.getLastCard()).append("\n");
         s.append(current_player.getTurnInfo());
         return s.toString();
+    }
+    public String processTurn() { //todo: build a String that narrates the actions being taken
+        StringBuilder s = new StringBuilder();
+        Player current_player = players[index];
+        Card c = current_player.askForCard(deck.getLastCard());
+        if(c == null) {
+            current_player.addCard(deck.takeCard());
+            s.append(current_player.name).append(": taking card from deck\n").append(processTurn());
+        }
+        else {
+            s.append(current_player.name).append(": placed a ").append(c);
+            if(c instanceof ActionCard ac) {
+                s.append(ac.doAction(this));
+            }
+            deck.putCard(c);
+        }
+        return s.toString();
+    }
+
+    public void reverse() {
+        reverse = !reverse;
+    }
+
+    public void draw(int i) {
+        Player victim = players[index + 1];
+        for(int j = 0; j < i; j++) {
+            victim.addCard(deck.takeCard());
+        }
+    }
+
+    public colors changeColor() {
+        return players[index].askForColor();
     }
 }
