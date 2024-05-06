d16794c9f3c5feaf5fa3be71f57f0e9a3a3297da
Tyler <tylerrimkunas@gmail.com>
2024-05-06 16:32:10 -0400

    ChangeColorCard now has a fake color that gets changed and used when matched. overrides getColor().

--- src/ChangeColorCard.java
+++ src/ChangeColorCard.java
@@ -1,12 +1,19 @@
 public class ChangeColorCard extends ActionCard{
+    protected colors fake_color;
     public ChangeColorCard(colors color, values value) {
         super(color, value);
+        fake_color = null;
     }
 
     @Override
     public String doAction(Game g) {
-        color = g.changeColor();
-        return "The color is now " + color + "\n";
+        fake_color = g.changeColor();
+        return "The color is now " + fake_color + "\n";
+    }
+
+    @Override
+    public colors getColor() {
+        return fake_color;
     }
 
 }
