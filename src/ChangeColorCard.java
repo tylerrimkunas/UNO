5668dbda298c3c002ae989dac3689a2afbf0cdbc
Tyler <tylerrimkunas@gmail.com>
2024-05-06 17:15:14 -0400

    fake_color will be WILD instead of null

--- src/ChangeColorCard.java
+++ src/ChangeColorCard.java
@@ -2,7 +2,7 @@ public class ChangeColorCard extends ActionCard{
     protected colors fake_color;
     public ChangeColorCard(colors color, values value) {
         super(color, value);
-        fake_color = null;
+        fake_color = colors.WILD;
     }
 
     @Override
